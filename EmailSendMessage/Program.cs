ad8e07c3f95ed00a4d7afd12bb158940cda48f37
Антон Бойко <energyone.tony@mail.ru>
2021-09-02 01:53:22 +0300

    Получилось! Мэил отправляется!

--- EmailSendMessage/Program.cs
+++ EmailSendMessage/Program.cs
@@ -14,9 +14,9 @@ namespace EmailSendMessage
         private static async Task SendEmailAsync()
         {
             // отправитель - устанавливаем адрес и отображаемое в письме имя
-            MailAddress from = new MailAddress("energyone.tony@mail.ru", "AntonEnergy");
+            MailAddress from = new MailAddress("energyone.tony@yandex.ru", "AntonEnergy");
             // кому отправляем
-            MailAddress to = new MailAddress("antonmega95@mail.ru");
+            MailAddress to = new MailAddress("energyone.tony@mail.ru");
             // создаем объект сообщения
             MailMessage m = new MailMessage(from, to);
             // тема письма
@@ -26,19 +26,35 @@ namespace EmailSendMessage
             // письмо представляет код html
             m.IsBodyHtml = true;
             // адрес smtp-сервера и порт, с которого будем отправлять письмо
-            SmtpClient smtp = new SmtpClient("smtp.mail.ru", 465);
+            SmtpClient smtp = new SmtpClient("smtp.yandex.ru", 465);
             // логин и пароль
-            smtp.Credentials = new NetworkCredential("", "");
+            smtp.Credentials = new NetworkCredential("energyone.tony@yandex.ru", "Veo48764511");
             smtp.EnableSsl = true;
             await smtp.SendMailAsync(m);
             Console.WriteLine("Всё получилось!");
         }
         public static void Main(string[] args)
         {
-            SendEmailAsync().GetAwaiter();
-            Console.WriteLine("Всё получилось?");
+            // отправитель - устанавливаем адрес и отображаемое в письме имя
+            MailAddress from = new MailAddress("energyone.tony@mail.ru", "AntonEnergy");
+            // кому отправляем
+            MailAddress to = new MailAddress("energyone.tony@mail.ru");
+            // создаем объект сообщения
+            MailMessage m = new MailMessage(from, to);
+            // тема письма
+            m.Subject = "Если получится - пойду спать";
+            // текст письма
+            m.Body = "<h2>Осталось совсем чуть-чуть и всё заработает...</h2>";
+            // письмо представляет код html
+            m.IsBodyHtml = true;
+            // адрес smtp-сервера и порт, с которого будем отправлять письмо
+            SmtpClient smtp = new SmtpClient("smtp.mail.ru", 2525);
+            // логин и пароль
+            smtp.Credentials = new NetworkCredential("*", "*");
+            smtp.EnableSsl = true;
+            smtp.Send(m);
+            Console.WriteLine("Всё получилось!");
             Console.ReadKey();
-            
         }
     }
 }
